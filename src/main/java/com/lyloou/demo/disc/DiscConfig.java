db8eea50b14479e64a657e3213b254cecf72e023
lilou <lyloou@qq.com>
2018-09-29 17:19:49 +0800

    update disc

--- src/main/java/com/lyloou/demo/disc/DiscConfig.java
+++ src/main/java/com/lyloou/demo/disc/DiscConfig.java
@@ -1,14 +1,17 @@
 package com.lyloou.demo.disc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:config/disc.properties")
 public class DiscConfig {
+
 
     @Autowired
     Environment env;
@@ -16,8 +19,24 @@ public class DiscConfig {
     @Bean
     public Disc disc() {
         Disc disc = new Disc();
+        System.out.println("==> use environment: env");
         disc.setTitle(env.getProperty("disc.title"));
         disc.setArtist(env.getProperty("disc.artist"));
         return disc;
+    }
+
+    @Bean
+    public Disc discEnv(@Value("${disc.title}") String title,
+                        @Value("${disc.artist}") String artist) {
+        System.out.println("==> use annotation: value");
+        Disc disc = new Disc();
+        disc.setTitle(title);
+        disc.setArtist(artist);
+        return disc;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
