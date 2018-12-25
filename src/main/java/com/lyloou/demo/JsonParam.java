af34f61ef352b82762af1771367ebeef2411b003
lilou <lyloou@qq.com>
2018-12-25 16:37:38 +0800

    add json controller

---
+++ src/main/java/com/lyloou/demo/JsonParam.java
@@ -0,0 +1,31 @@
+package com.lyloou.demo;
+
+public class JsonParam {
+    private String name;
+    private int count;
+    private String note;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
--- src/main/java/com/lyloou/demo/MyController.java
+++ src/main/java/com/lyloou/demo/MyController.java
@@ -2,8 +2,7 @@ package com.lyloou.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller // 标识为控制器
 public class MyController {
@@ -14,5 +13,21 @@ public class MyController {
         model.addAttribute("message", "Hello World!!");
         // 返回逻辑视图名称
         return "index";
+    }
+
+    @GetMapping("/json")
+    @ResponseBody
+    public ResultInfo json() {
+        return new ResultInfo(true, "ok");
+    }
+
+    @PostMapping("/json/object")
+    @ResponseBody
+    public ResultInfo jsonObject(@RequestBody JsonParam jsonParam) {
+        String msg = String.format(" name:%s \n count: %d\n note: %s",
+                jsonParam.getName(),
+                jsonParam.getCount(),
+                jsonParam.getNote());
+        return new ResultInfo(true, msg);
     }
 }
